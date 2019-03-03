76fa0003a2a5f66c83029e5a38448c9ff047dd37
ZeyingZhou <47362333+ZeyingZhou@users.noreply.github.com>
2019-03-03 14:05:18 -0500

    button list

---
+++ button list/buttonlist.js
@@ -0,0 +1,52 @@
+
+
+	var buttons = document.querySelectorAll("button");
+
+
+	$(".list").on("click", "button", function(){
+	// $(this).toggleClass("select");		
+	$(this).parent().siblings().children(".add").toggleClass("exclude")
+	});
+
+
+
+
+
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13) {
+		var todoText = $(this).val();
+		$(this).val("");
+		$(".list").append("<div><button class=\"add\"><span><i class=\"fa fa-trash\"></i></span> " + todoText + "</button></div>");
+	}
+});
+
+$(".list").on("click", "span", function(event){
+$(this).parent().fadeOut(500,function(){
+	$(this).remove();
+});
+event.stopPropagation();
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
+
+// var colors = ['#ff0000', '#00ff00', '#0000ff'];
+// var random_color = colors[Math.floor(Math.random() * colors.length)];
+// $("button").css('color', random_color);
+// buttons[0].style.backgroundColor = random_color; 
+ // $(document).ready(function()	
+ //            {
+ //                var myColors = new Array();
+ //                myColors[0] = "#2672EC";
+ //                myColors[1] = "#97009F";
+ //                myColors[2] = "#094DB5";
+ //                myColors[3] = "#00A300";
+ //                myColors[4] = "#DA532C";
+ //                myColors[5] = "#AF1A3F";
+ //                myColors[6] = "#613CBC";
+ //                myColors[7] = "#008AD2";
+
+ //                var rand = Math.floor(Math.random()*myColors.length); 
+ //                $('button').css("background-color", myColors[rand]); 
+ //            });
