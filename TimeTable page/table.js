ea549301019cf1762c583615ff061be7403fe384
ZeyingZhou <47362333+ZeyingZhou@users.noreply.github.com>
2019-03-06 14:09:15 -0500

    Updates for timetable page

--- TimeTable page/table.js
+++ TimeTable page/table.js
@@ -1,29 +1,28 @@
-	var buttons = document.querySelectorAll("button");
-
-	//button click function
+var buttons = document.querySelectorAll("button");
+     //button click function
+    
 $(".list").on("click", "button", function(){
 	$(this).toggleClass("select");		
-	$(this).parent().siblings().children(".add").toggleClass("exclude");
-	//make the button draggable
-	$(this).draggable({cancel:false, cursor: "crosshair", revert: "invalid"});
-	//hide other buttons when button is clicked
-	$(this).parent().siblings().children(".add").toggle(100);
-	//$(this).parent().siblings().children(".add").toggle(function(){
-	//$(this).attr("disabled");
-	//});
+	// $(this).parent().siblings().children(".add").toggleClass("exclude");
+	// $(this).children().children(".fa fa-trash").toggleAttr("class","fa fa-check","fa fa-trash");
+	$(this).draggable({cancel:false ,cursor: "crosshair", revert: "invalid"});
+	// $(this).toggle(function(){
+	// 	$(this).draggable({cancel:false},{ cursor: "crosshair", revert: "invalid"});
+	// });
+	// $(this).parent().siblings().children(".add").toggle(100);
+	// $(this).attr("disabled");
 });
 
-	//input create new buttons
+	//Input enter create buttons
 $("input[type='text']").keypress(function(event){
 	if (event.which === 13) {
 		var todoText = $(this).val();
 		$(this).val("");
-		//Send New buttons to the html 
 		$(".list").append("<div class=\"origin\"><button class=\"add\"><span><i class=\"fa fa-trash\"></i></span> " + todoText + "</button></div>");
 	}
 });
-
-	//button delete function
+	
+	//Delete button function
 $(".list").on("click", "span", function(event){
 $(this).parent().parent().fadeOut(500,function(){
 	$(this).remove();
@@ -31,15 +30,13 @@ $(this).parent().parent().fadeOut(500,function(){
 event.stopPropagation();
 });
 
-	//hide input box
+	//Hide input function
 $(".fa-plus").click(function(){
 	$("input[type='text']").fadeToggle();
 });
-
-//drag and drop function 
-//I have no idea what these codes meean
-// I just copied from others and made some changes 
-$(".drop").droppable({ accept: ".add", 
+  //drag and drop components
+// $(".add").draggable({ cursor: "crosshair", revert: "invalid"});
+$(".timeSlot").droppable({ accept: ".add", 
            drop: function(event, ui) {
                     console.log("drop");
                    $(this).removeClass("border").removeClass("over");
@@ -58,7 +55,7 @@ $(".drop").droppable({ accept: ".add",
                     $(this).removeClass("over");
                   }
                      });
-$(".drop").sortable();
+$(".timeSlot").sortable();
 
 $(".origin").droppable({ accept: ".add", drop: function(event, ui) {
                     console.log("drop");
@@ -67,5 +64,4 @@ $(".origin").droppable({ accept: ".add", drop: function(event, ui) {
             var droppedOn = $(this);
             $(dropped).detach().css({top: 0,left: 0}).appendTo(droppedOn);      
              
-             
- }});
+}});
