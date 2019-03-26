a2cc904e352d0128421509914ed4b53365845de5
Malcolm Wyllie <17mjdw@queensu.ca>
2019-03-26 11:43:36 -0400

    Update MainPage.js

--- Backend Code/MainPage.js
+++ Backend Code/MainPage.js
@@ -4,6 +4,9 @@ app.use(express.static("public"))
 app.get("/", function(req,res){
     res.render("landingPage.ejs");
     console.log("User made request to home")
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    listen(8080);
+console.log('Server running at http://localhost:8080/');
 });
 
 app.get("/table", function(req,res){
